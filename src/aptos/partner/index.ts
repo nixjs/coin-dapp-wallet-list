549459ecbcdd2e34fa226d878a8cd62b82b16ae8
nghinv <nghinv@inpirelab.io>
2022-12-09 16:56:49 +0700

    fix: url

--- src/aptos/partner/index.ts
+++ src/aptos/partner/index.ts
@@ -1,12 +1,11 @@
-import { Dapp } from "../../types";
+import { Dapp } from '../../types'
 
 export const Partner: Dapp[] = [
-  {
-    name: "SimpleSwap",
-    url: "https://simpleswap.io/",
-    description:
-      "SimpleSwap is a cryptocurrency exchange that allows to swap BTC and altcoins in an easy way.",
-    img: "https://raw.githubusercontent.com/nixjs/coin-dapp-wallet-list/main/aptos/partner/assets/SimpleSwap.svg",
-    category: "Exchange",
-  },
-];
+    {
+        name: 'SimpleSwap',
+        url: 'https://simpleswap.io/',
+        description: 'SimpleSwap is a cryptocurrency exchange that allows to swap BTC and altcoins in an easy way.',
+        img: 'https://raw.githubusercontent.com/nixjs/coin-dapp-wallet-list/main/src/aptos/partner/assets/SimpleSwap.svg',
+        category: 'Exchange',
+    },
+]
