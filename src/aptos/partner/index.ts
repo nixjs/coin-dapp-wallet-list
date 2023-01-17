9226342163c09a5a9a1144063a0bc46c72b2bc97
nghinv <nghinv@inpirelab.io>
2023-01-17 19:23:40 +0700

    feat: add stealthEx

--- src/aptos/partner/index.ts
+++ src/aptos/partner/index.ts
@@ -4,8 +4,17 @@ export const Partner: Dapp[] = [
     {
         name: 'SimpleSwap',
         url: 'https://simpleswap.io/',
-        description: 'SimpleSwap is a cryptocurrency exchange that allows to swap BTC and altcoins in an easy way.',
+        description:
+            'SimpleSwap is a user-friendly and reliable service for cryptocurrency exchanges. Our platform is free from sign-up and supports more than 500 crypto and fiat currencies.',
         img: 'https://raw.githubusercontent.com/nixjs/coin-dapp-wallet-list/main/src/aptos/partner/assets/SimpleSwap.svg',
+        category: 'Exchange',
+    },
+    {
+        name: 'StealthEX',
+        url: 'https://stealthex.io/',
+        description:
+            'StealthEX is an instant cryptocurrency exchange for limitless swaps. Their service is free from registration and does not store user’s funds on the platform. StealthEX is the best place to buy cryptocurrency.',
+        img: 'https://raw.githubusercontent.com/nixjs/coin-dapp-wallet-list/main/src/aptos/partner/assets/StealthEX.png',
         category: 'Exchange',
     },
 ]
